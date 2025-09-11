432b5fc8603106745961b562b0af65c6ae70fbcb
mahto2112 <ashkumar893@gmail.com>
2025-09-11 12:30:18 +0530

    Add files via upload

---
+++ assign.02.5.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    int c;
+    int sum;
+
+    printf("Enter your numbers");
+    scanf("%d%d%d",&a,&b,&c);
+
+    sum=a+b+c;
+
+    printf("sum of total:" "%d",sum );
+
+
+
+
+
+
+
+
+
+}
---
+++ assign03.1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int a;
+
+
+
+    printf("Enter year");
+    scanf("%d",&a);
+
+    if(a%4==0){
+        printf("Is a leap year");
+    } if (a%4!=0){
+        printf("Is not a leap year");
+    }
+
+
+
+
+
+
+
+
+
+
+}
